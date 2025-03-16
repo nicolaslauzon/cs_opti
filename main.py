904578db2c1316d4d7b7cdd66da08b2b07bc1181
Thomas Couturier <thomas.couturier0330@gmail.com>
2025-03-16 10:27:24 -0400

    Added load function for day 2

--- main.py
+++ main.py
@@ -6,20 +6,42 @@ DAY1_FILENAME = "./data/day_1.json"
 DAY2_FILENAME = "./data/day_2.json"
 DAY3_FILENAME = "./data/day_3.json"
 
+customers, warehouses, matrix = parse_las_brickas()
 
 def day1():
-    customers, warehouses, matrix = parse_las_brickas()
+    #Fait a la main
     warehouses, customers, trucks, box = parse_day(DAY1_FILENAME, warehouses, customers)
     print(box)
+
+def day2():
+    warehouses, customers, trucks, box = parse_day(DAY2_FILENAME, warehouses, customers)
+    steps = addAllLoadsForDay2(steps, customers)
+    e = 3
+
+def day3():
+    steps = []
+    warehouses, customers, trucks, box = parse_day(DAY3_FILENAME, warehouses, customers)
+    e = 3
 
 def getDistManhattan(x1, y1, x2, y2): 
     xDiff = abs(x2 - x1)
     yDiff = abs(y2 - y1)
     return xDiff + yDiff
 
+def addAllLoadsForDay2(steps, customers):
+    orderDict = {}
+    for customer in customers: 
+        for order in customer["Orders"]:
+            if order["LegoId"] in orderDict:
+                orderDict[order["LegoId"]] += 1
+            else:
+                orderDict[order["LegoId"]] = 1
+    for legoType in orderDict:
+        steps = addLoadActionToAnswer(0, orderDict[legoType], legoType, steps)
+    return steps
 
 def addLoadActionToAnswer(truck, quantity, lego, steps):
-    steps.append("load truck=" + truck + "  quantity=" + quantity + " lego=" + lego)
+    steps.append("load truck=" + str(truck) + " quantity=" + str(quantity) + " lego=" + str(lego))
     return steps
 
 
